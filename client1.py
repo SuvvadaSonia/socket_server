eae361734a3c330175d5e2e52b96fdf77a824ce0
SuvvadaSonia <suvvadasonia@gmail.com>
2022-07-28 21:36:27 +0530

    Added comments

--- client1.py
+++ client1.py
@@ -1,5 +1,16 @@
-import socket
-s = socket.socket()
-h=socket.gethostname()
-s.connect((h,9999))
+"""
+Description: A simple connection between socket server and socket client
+using socket module
+Author: Sonia
+Position: Junior Software Engineer
+
+"""
+
+import socket # import a socket module
+
+
+s = socket.socket() # create a socket object
+h=socket.gethostname() # get the local machine name
+s.connect((h,9999)) # reserve a port for your connection service and connects to that 
 print(s.recv(1024))
+s.close() # close the socket when done
--- server1.py
+++ server1.py
@@ -1,11 +1,22 @@
-import socket
-s=socket.socket()
-h=socket.gethostname()
-s.bind((h,9999))
-s.listen(3)
+"""
+Description: A simple connection between socket server and socket client
+using socket module
+Author: Sonia
+Position: Junior Software Engineer
+
+"""
+
+import socket # import the socket module
+
+
+s=socket.socket() # create a socket object
+h=socket.gethostname() # Get local machine name
+s.bind((h,9999)) # Reserve a port for your service and bind to that particular port
+s.listen(3) # Now wait for client connection
+
 while True:
-    c,addr = s.accept()
+    c,addr = s.accept() # Establish connection with client
     print("Got connection from ",addr)
-    c.send(b'Thank you for connecting')
-    c.close()
+    c.send(b'Thank you for connecting') # Send message to client after connection
+    c.close() # close the connection
     
